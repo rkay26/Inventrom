cef44bf220c81fc1ec70441ab6adf1e4bc305eb9
rkay26 <30926420+rkay26@users.noreply.github.com>
2017-10-13 16:27:11 +0530

    Update Humidity.py
    Using Library Plotly to show the visualization

--- Humidity.py
+++ Humidity.py
@@ -1,5 +1,7 @@
 import requests
 import numpy as np
+from plotly.offline import init_notebook_mode, plot
+import plotly.graph_objs as go
 
 if __name__ == '__main__':
 
@@ -27,3 +29,58 @@ if __name__ == '__main__':
     # Creating Numpy arrays
     time_x = np.array(time_stamp)
     humidity_y = np.array(humidity_value)
+ 
+   # Describe Layout of the graph
+    plot_data = [trace0]
+    init_notebook_mode(connected=True)
+    title_Graph = 'Time-stamp v/s Humidity Level (' + device_name + ')'
+    layout = go.Layout(
+                title=title_Graph,
+                showlegend=True,
+                height=600,
+                width=1350,
+                yaxis = dict(
+                            title='Humidity Level',
+                            zeroline = False,
+                            autorange=True,
+                            ticks='outside',
+                            tick0=0,
+                            ticklen=8,
+                            tickwidth=4,
+                            tickcolor='#000',
+                            titlefont = dict(
+                                            size = 16        
+                                        ),
+                            tickfont = dict(
+                                            size = 11
+                                        )
+                        ),
+                xaxis = dict(
+                            title='Time',
+                            zeroline = False,
+                            autorange=True,
+                            ticks='outside',
+                            tick0=0,
+                            ticklen=8,
+                            tickwidth=4,
+                            tickcolor='#000',
+                            titlefont = dict(
+                                            size = 16       
+                                        ),
+                            tickfont = dict(
+                                            size = 11
+                                        ),
+                            tickmode = 'auto',
+                            tickangle = 90
+                        ),
+                font = dict(
+                            family='Roboto', 
+                            size=20, 
+                            color='#5e2222'
+                        ),
+            )
+    
+    # Show graph
+    fig = dict(data=plot_data, layout=layout )
+    plot(fig)
+    
